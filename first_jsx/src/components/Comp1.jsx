8ee771cf1019c8413be063d573cbc0d8b8cf74f5
Rachel Howell <rhowell96@gmail.com>
2022-10-27 13:39:45 -0700

    Create first react app

---
+++ first_jsx/src/components/Comp1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Comp1 = () => {
+    return (
+        <div>
+            <div><h1>Hello Dojo!</h1></div>
+            <div><h2>Things I need to do:</h2></div>
+            <div>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the cats</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Comp1

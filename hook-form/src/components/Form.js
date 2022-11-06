68a3c8f5bbf14698527d6cf31f5f246ba102d2e9
Rachel Howell <rhowell96@gmail.com>
2022-11-06 15:19:35 -0800

    Add validations and replace if/else statements with ternary operators

--- hook-form/src/components/Form.js
+++ hook-form/src/components/Form.js
@@ -2,35 +2,92 @@ import React, { useState } from 'react'
 
 const Form = () => {
 
-    const [ firstName, setFirstName ] = useState("");
-    const [ lastName, setLastName ] = useState("");
-    const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
-    const [ confirmPassword, setConfirmPassword ] = useState("");
+    const [ firstError, setFirstError ] = useState("");
+    const [ lastError, setLastError ] = useState("");
+    const [ emailError, setEmailError ] = useState("");
+    const [ passwordError, setPasswordError ] = useState("");
+    const [ confirmPasswordError, setConfirmPasswordError ] = useState("");
+
+
+    const handleFirst = (e) => {
+        e.preventDefault();
+        e.target.value.length < 2 ?
+        setFirstError("First Name must be at least 2 characters.") :
+        setFirstError("");
+    }
+
+    const handleLast = (e) => {
+        e.preventDefault();
+        e.target.value.length < 2 ?
+        setLastError("Last Name must be at least 2 characters.") :
+        setLastError("");
+    }
+
+    const handleEmail = (e) => {
+        e.preventDefault();
+        e.target.value.length < 5 ?
+        setEmailError("Email must be at least 5 characters.") :
+        setEmailError("");
+    }
+
+    const handlePassword = (e) => {
+        e.preventDefault();
+        setPassword(e.target.value);
+        e.target.value.length < 8 ?
+        setPasswordError("Password must be at least 8 characters.") :
+        setPasswordError("");
+    }
+
+    const handleConfirmPassword = (e) => {
+        e.preventDefault();
+        e.target.value != password ?
+        setConfirmPasswordError("Passwords must match.") :
+        setConfirmPasswordError("");
+    }
+    
+
 
     return (
         <div className="border col-4 mx-auto">
             <form className="p-3">
                 <label className="form-label">First Name: </label>
-                <input type="text" onChange={ (e) => setFirstName(e.target.value) }/>
+                <input type="text" onChange={ handleFirst }/>
+                {
+                    firstError ?
+                    <p>{ firstError }</p> :
+                    ''
+                }
                 <label className="form-label pt-3">Last Name: </label>
-                <input type="text" onChange={ (e) => setLastName(e.target.value) }/>
+                <input type="text" onChange={ handleLast }/>
+                {
+                    lastError ?
+                    <p>{ lastError }</p> :
+                    ''
+                }
                 <label className="form-label pt-3">Email: </label>
-                <input type="text" onChange={ (e) => setEmail(e.target.value) }/>
+                <input type="text" onChange={ handleEmail }/>
+                {
+                    emailError ?
+                    <p> { emailError }</p> :
+                    ''
+                }
                 <label className="form-label pt-3">Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) }/>
+                <input type="text" onChange={ handlePassword }/>
+                {
+                    passwordError ?
+                    <p> { passwordError }</p> :
+                    ''
+                }
                 <label className="form-label pt-3">Confirm Password: </label>
-                <input type="text" onChange={ (e) => setConfirmPassword(e.target.value) }/>
+                <input type="text" onChange={ handleConfirmPassword }/>
+                {
+                    confirmPasswordError ?
+                    <p> { confirmPasswordError }</p> :
+                    ''
+                }
                 <button className="btn mx-auto border mt-3" type="submit">Submit</button>
             </form>
-            <div>
-                <h2>Your Form Data</h2>
-                <h5 className="text-start ps-5">First Name: { firstName } </h5>
-                <h5 className="text-start ps-5">Last Name: { lastName } </h5>
-                <h5 className="text-start ps-5">Email: { email } </h5>
-                <h5 className="text-start ps-5">Password: { password } </h5>
-                <h5 className="text-start ps-5">Confirm Password: { confirmPassword } </h5>
-            </div>
         </div>
     )  
 }

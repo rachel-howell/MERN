53bd046bb859085432da41fde11d3523cfae70b8
Rachel Howell <rhowell96@gmail.com>
2022-11-16 18:12:35 -0800

    Update API call with Axios

--- poke-api/src/components/AshKetchum.js
+++ poke-api/src/components/AshKetchum.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const AshKetchum = () => {
 
   const [ pokemonList, setPokemonList ] = useState([]);
 
+  // useEffect(() => {
+  //   fetch("https://pokeapi.co/api/v2/pokemon?limit=10000")
+  //         .then(response => {
+  //           return response.json();
+  //       }).then(response => {
+  //         setPokemonList(
+  //           response.results
+  //         );
+  //       }).catch(err=>{
+  //           console.log(err);
+  //       });
+  // }, [])
+
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=10000")
-          .then(response => {
-            return response.json();
-        }).then(response => {
-          setPokemonList(
-            response.results
-          );
-        }).catch(err=>{
-            console.log(err);
-        });
+    console.log("Gotta catch em all");
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000')
+    .then(response=>{
+      console.log(response);
+      setPokemonList(response.data.results);
+    });
   }, [])
 
+  console.log(pokemonList)
 
   return (
     <div>
